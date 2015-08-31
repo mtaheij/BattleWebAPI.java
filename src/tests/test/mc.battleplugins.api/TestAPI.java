d0ecfab6c181143f1d3a4bbeda4b1835d89cbce6
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-08-31 00:03:54 -0500

    Fix tests
    - name = BattleWebAPI
    - added junit dependency
    - declare testSourceDirectory: ./src/tests
    - fix test directory declaration: "./test_files"
    - comment out method testStats() because this API currently does not exist
      on the website.
    - distribution repository NOT updated.

--- src/tests/test/mc.battleplugins.api/TestAPI.java
+++ src/tests/test/mc.battleplugins.api/TestAPI.java
@@ -13,26 +13,25 @@ import java.util.Map;
 import java.util.Random;
 
 public class TestAPI extends TestCase {
-    String testDir = System.getProperty("user.home") +
-            "/workspace/mc/BattlePluginsAPI/test_files";
-    String configFile = testDir+"/TestConfig.yml";
+    String testDir = "./test_files";
+    String configFile = testDir + "/TestConfig.yml";
 
     public void testPaste() throws IOException {
         BattlePluginsAPI api = new TestBattlePluginsAPI("TestServer", "1.0", configFile);
-        api.pasteFile("TestAPI", testDir+"/TestPasteFile.txt");
+        api.pasteFile("TestAPI", testDir + "/TestPasteFile.txt");
     }
 
-    public void testStats() throws IOException {
-        TestBattlePluginsAPI api = new TestBattlePluginsAPI("TestServer", "1.0", configFile);
-        api.set();
-        api.setPlayersOnline(new Random().nextInt(10));
-        TestPlugin tp = new TestPlugin("TestPlugin", "1.0");
-        TestPlugin tp2 = new TestPlugin("TestPlugin2", "2.0");
-        List<Plugin> l = new ArrayList<Plugin>();
-        l.add(tp);    /** battleplugins site */
-        l.add(tp2);
-        api.sendStatistics(l);
-    }
+//    public void testStats() throws IOException {
+//        TestBattlePluginsAPI api = new TestBattlePluginsAPI("TestServer", "1.0", configFile);
+//        api.set();
+//        api.setPlayersOnline(new Random().nextInt(10));
+//        TestPlugin tp = new TestPlugin("TestPlugin", "1.0");
+//        TestPlugin tp2 = new TestPlugin("TestPlugin2", "2.0");
+//        List<Plugin> l = new ArrayList<Plugin>();
+//        l.add(tp);    /** battleplugins site */
+//        l.add(tp2);
+//        api.sendStatistics(l);
+//    }
 
     Map<String,Object> stringToMap(String str) {
         Map<String,Object> map = new HashMap<String, Object>();
